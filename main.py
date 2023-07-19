e33beece02f28be9dedbf9e04a071a7e1c3374f0
IdleGoat <rafie.amandio@ui.ac.id>
2023-07-19 10:17:10 +0800

    fix : error for aruba below 3.9.0

--- main.py
+++ main.py
@@ -32,38 +32,40 @@ if __name__ == '__main__':
     ARUBA_USERNAME = os.getenv('ARUBA_USERNAME')
     ARUBA_PASSWORD = os.getenv('ARUBA_PASSWORD')
     ARUBA_IPADDRESS = os.getenv('ARUBA_IPADDRESS')
-    print(ARUBA_USERNAME)
-    print(ARUBA_PASSWORD)
-    print(ARUBA_IPADDRESS)
 
     while True:
         data_rows = {}
         for ap_name in ap_names:
             for essid, chan in data_rows.keys():
                 data_rows[(essid, chan)][f"rssi_{ap_name}"] = ''
-            token = get_aruba_id(
-                ARUBA_IPADDRESS,
-                ARUBA_USERNAME,
-                ARUBA_PASSWORD)
+            # token = get_aruba_id(
+            #     ARUBA_IPADDRESS,
+            #     ARUBA_USERNAME,
+            #     ARUBA_PASSWORD)
             command = 'show+ap+monitor+ap-list+ap-name+' + ap_name
-            list_ap_database = list_show_command(
-                ARUBA_IPADDRESS, token, command)
-            # list_ap_database = list_show_command_test(ap_name)
+            # list_ap_database = list_show_command(
+            #     ARUBA_IPADDRESS, token, command)
+            list_ap_database = list_show_command_test(ap_name)
             ap_data = list_ap_database['Monitored AP Table']
 
             for monitored_ap in ap_data:
                 monitored_ap['ap_name'] = ap_name
                 monitored_ap['timestamp'] = time.time()
                 monitored_ap['count'] = count
-
                 essid = monitored_ap['essid']
-                chan = monitored_ap['chan']
+
+                if 'band/chan/ch-width/ht-type' in monitored_ap:
+                    band, chan, ch_width, ht_type = parse_data(
+                        monitored_ap['band/chan/ch-width/ht-type'])
+                else:
+                    chan = monitored_ap['chan']
+                    band = ''
 
                 rssi_key = f"rssi_{ap_name}"
 
                 if (essid, chan) not in data_rows:
                     data_rows[(essid, chan)] = {
-                        'count': count, 'bssid': monitored_ap['bssid'], 'chan': monitored_ap['chan'], 'phy-type': monitored_ap['phy-type']}
+                        'count': count, 'bssid': monitored_ap['bssid'], 'chan': chan, 'band': band}
 
                 data_rows[(essid, chan)][rssi_key] = monitored_ap['curr-rssi']
 
